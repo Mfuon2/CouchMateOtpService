d6217a214c6d66f6403f7ab164ddee5dc8e65726
Mfuon <leonard.mfuon@icealion.com>
2020-03-13 18:08:44 +0300

    App start add Middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import * as dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
+
+
 app.listen(5000, () => {
     //TODO: Add env with the port number
     console.log(`Listening on ${5000}`);
