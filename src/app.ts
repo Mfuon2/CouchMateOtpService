d3d62765900712f3a5d59c8c292669acf22ddaa3
Mfuon <leonard.mfuon@icealion.com>
2020-03-16 23:58:00 +0300

    types fix

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 // @ts-ignore
 import * as express from 'express';
+// @ts-ignore
 import * as dotenv from 'dotenv';
+// @ts-ignore
 import * as logger from '../src/middlewares/logger';
+// @ts-ignore
 import * as routes from '../src/routes/user';
 
 const app = express();
